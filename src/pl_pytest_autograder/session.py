35796742d56a89d54af461250bbf15268ac00c90
Eliot Robson <eliot.robson24@gmail.com>
2025-05-24 11:51:54 -0500

    Removed histogram file

--- src/pl_pytest_autograder/session.py
+++ src/pl_pytest_autograder/session.py
@@ -27,8 +27,8 @@ class BenchmarkSession:
     groups = None
 
     def __init__(self, config):
-        self.verbose = config.getoption('benchmark_verbose')
-        self.quiet = False if self.verbose else config.getoption('benchmark_quiet')
+        self.verbose = config.getoption("benchmark_verbose")
+        self.quiet = False if self.verbose else config.getoption("benchmark_quiet")
         level = Logger.QUIET if self.quiet else Logger.NORMAL
         if self.verbose:
             level = Logger.VERBOSE
@@ -38,48 +38,48 @@ class BenchmarkSession:
         self.benchmarks = []
         self.machine_id = get_machine_id()
         self.storage = load_storage(
-            config.getoption('benchmark_storage'),
+            config.getoption("benchmark_storage"),
             logger=self.logger,
             default_machine_id=self.machine_id,
-            netrc=config.getoption('benchmark_netrc'),
+            netrc=config.getoption("benchmark_netrc"),
         )
-        self.cprofile_sort_by = config.getoption('benchmark_cprofile')
-        self.cprofile_loops = config.getoption('benchmark_cprofile_loops')
-        self.cprofile_top = config.getoption('benchmark_cprofile_top')
-        self.cprofile_dump = first_or_value(config.getoption('benchmark_cprofile_dump'), False)
+        self.cprofile_sort_by = config.getoption("benchmark_cprofile")
+        self.cprofile_loops = config.getoption("benchmark_cprofile_loops")
+        self.cprofile_top = config.getoption("benchmark_cprofile_top")
+        self.cprofile_dump = first_or_value(config.getoption("benchmark_cprofile_dump"), False)
         self.options = {
-            'min_time': SecondsDecimal(config.getoption('benchmark_min_time')),
-            'min_rounds': config.getoption('benchmark_min_rounds'),
-            'max_time': SecondsDecimal(config.getoption('benchmark_max_time')),
-            'timer': load_timer(config.getoption('benchmark_timer')),
-            'calibration_precision': config.getoption('benchmark_calibration_precision'),
-            'disable_gc': config.getoption('benchmark_disable_gc'),
-            'warmup': config.getoption('benchmark_warmup'),
-            'warmup_iterations': config.getoption('benchmark_warmup_iterations'),
-            'cprofile': bool(self.cprofile_sort_by),
-            'cprofile_loops': self.cprofile_loops,
-            'cprofile_dump': self.cprofile_dump,
+            "min_time": SecondsDecimal(config.getoption("benchmark_min_time")),
+            "min_rounds": config.getoption("benchmark_min_rounds"),
+            "max_time": SecondsDecimal(config.getoption("benchmark_max_time")),
+            "timer": load_timer(config.getoption("benchmark_timer")),
+            "calibration_precision": config.getoption("benchmark_calibration_precision"),
+            "disable_gc": config.getoption("benchmark_disable_gc"),
+            "warmup": config.getoption("benchmark_warmup"),
+            "warmup_iterations": config.getoption("benchmark_warmup_iterations"),
+            "cprofile": bool(self.cprofile_sort_by),
+            "cprofile_loops": self.cprofile_loops,
+            "cprofile_dump": self.cprofile_dump,
         }
-        self.skip = config.getoption('benchmark_skip')
-        self.disabled = config.getoption('benchmark_disable') and not config.getoption('benchmark_enable')
+        self.skip = config.getoption("benchmark_skip")
+        self.disabled = config.getoption("benchmark_disable") and not config.getoption("benchmark_enable")
 
-        if config.getoption('dist', 'no') != 'no' and not self.skip and not self.disabled:
+        if config.getoption("dist", "no") != "no" and not self.skip and not self.disabled:
             self.logger.warning(
-                'Benchmarks are automatically disabled because xdist plugin is active.'
-                'Benchmarks cannot be performed reliably in a parallelized environment.',
+                "Benchmarks are automatically disabled because xdist plugin is active."
+                "Benchmarks cannot be performed reliably in a parallelized environment.",
             )
             self.disabled = True
-        if hasattr(config, 'slaveinput'):
+        if hasattr(config, "slaveinput"):
             self.disabled = True
         if not statistics and not self.disabled:
             self.logger.warning(
-                f'Benchmarks are automatically disabled because we could not import `statistics`\n\n{statistics_error}',
+                f"Benchmarks are automatically disabled because we could not import `statistics`\n\n{statistics_error}",
             )
             self.disabled = True
 
-        self.only = config.getoption('benchmark_only')
-        self.sort = config.getoption('benchmark_sort')
-        self.columns = config.getoption('benchmark_columns')
+        self.only = config.getoption("benchmark_only")
+        self.sort = config.getoption("benchmark_sort")
+        self.columns = config.getoption("benchmark_columns")
         if self.skip and self.only:
             self.skip = False
         if self.disabled and self.only:
@@ -87,15 +87,15 @@ class BenchmarkSession:
                 "Can't have both --benchmark-only and --benchmark-disable options. Note that --benchmark-disable is "
                 "automatically activated if xdist is on or you're missing the statistics dependency."
             )
-        self.group_by = config.getoption('benchmark_group_by')
-        self.save = config.getoption('benchmark_save')
-        self.autosave = config.getoption('benchmark_autosave')
-        self.save_data = config.getoption('benchmark_save_data')
-        self.json = config.getoption('benchmark_json')
-        self.compare = config.getoption('benchmark_compare')
-        self.compare_fail = config.getoption('benchmark_compare_fail')
-        self.name_format = NAME_FORMATTERS[config.getoption('benchmark_name')]
-        self.histogram = first_or_value(config.getoption('benchmark_histogram'), False)
+        self.group_by = config.getoption("benchmark_group_by")
+        self.save = config.getoption("benchmark_save")
+        self.autosave = config.getoption("benchmark_autosave")
+        self.save_data = config.getoption("benchmark_save_data")
+        self.json = config.getoption("benchmark_json")
+        self.compare = config.getoption("benchmark_compare")
+        self.compare_fail = config.getoption("benchmark_compare_fail")
+        self.name_format = NAME_FORMATTERS[config.getoption("benchmark_name")]
+        self.histogram = first_or_value(config.getoption("benchmark_histogram"), False)
 
     def get_machine_info(self):
         obj = self.config.hook.pytest_benchmark_generate_machine_info(config=self.config)
@@ -111,9 +111,9 @@ class BenchmarkSession:
                         compared = compared_mapping[bench.fullname]
                         source = short_filename(path, self.machine_id)
                         flat_bench = bench.as_dict(include_data=False, stats=False, cprofile=(self.cprofile_sort_by, self.cprofile_top))
-                        flat_bench.update(compared['stats'])
-                        flat_bench['path'] = str(path)
-                        flat_bench['source'] = source
+                        flat_bench.update(compared["stats"])
+                        flat_bench["path"] = str(path)
+                        flat_bench["source"] = source
                         if self.compare_fail:
                             for check in self.compare_fail:
                                 fail = check.fails(bench, flat_bench)
@@ -121,21 +121,21 @@ class BenchmarkSession:
                                     self.performance_regressions.append((self.name_format(flat_bench), fail))
                         yield flat_bench
                 flat_bench = bench.as_dict(include_data=False, flat=True, cprofile=(self.cprofile_sort_by, self.cprofile_top))
-                flat_bench['path'] = None
-                flat_bench['source'] = compared and 'NOW'
+                flat_bench["path"] = None
+                flat_bench["source"] = compared and "NOW"
                 yield flat_bench
 
     def save_json(self, output_json):
         with self.json as fh:
             fh.write(safe_dumps(output_json, ensure_ascii=True, indent=4).encode())
-        self.logger.info(f'Wrote benchmark data in: {self.json}', purple=True)
+        self.logger.info(f"Wrote benchmark data in: {self.json}", purple=True)
 
     def handle_saving(self):
         save = self.save or self.autosave
         if save or self.json:
             if not self.benchmarks:
                 if not self.disabled:
-                    self.logger.warning('Not saving anything, no benchmarks have been run!')
+                    self.logger.warning("Not saving anything, no benchmarks have been run!")
                 return
             machine_info = self.get_machine_info()
             commit_info = self.config.hook.pytest_benchmark_generate_commit_info(config=self.config)
@@ -184,7 +184,7 @@ class BenchmarkSession:
                 if self.compare is True:
                     msg += ". Can't load the previous benchmark."
                 else:
-                    msg += f' match {self.compare!r}.'
+                    msg += f" match {self.compare!r}."
                 self.logger.warning(msg)
 
             machine_info = self.get_machine_info()
@@ -195,8 +195,8 @@ class BenchmarkSession:
                     machine_info=machine_info,
                     compared_benchmark=compared_benchmark,
                 )
-                compared_mapping[path] = {bench['fullname']: bench for bench in compared_benchmark['benchmarks']}
-                self.logger.info(f'Comparing against benchmarks from: {path}', newline=False)
+                compared_mapping[path] = {bench["fullname"]: bench for bench in compared_benchmark["benchmarks"]}
+                self.logger.info(f"Comparing against benchmarks from: {path}", newline=False)
         self.compared_mapping = compared_mapping
 
     def finish(self):
@@ -229,33 +229,33 @@ class BenchmarkSession:
 
     def check_regressions(self):
         if self.compare_fail and not self.compared_mapping:
-            raise pytest.UsageError('--benchmark-compare-fail requires valid --benchmark-compare.')
+            raise pytest.UsageError("--benchmark-compare-fail requires valid --benchmark-compare.")
 
         if self.performance_regressions:
             self.logger.error(
-                'Performance has regressed:\n{}'.format('\n'.join('\t{} - {}'.format(*line) for line in self.performance_regressions))
+                "Performance has regressed:\n{}".format("\n".join("\t{} - {}".format(*line) for line in self.performance_regressions))
             )
-            raise PerformanceRegression('Performance has regressed.')
+            raise PerformanceRegression("Performance has regressed.")
 
     def display_cprofile(self, tr):
         section_displayed = False
         for group in self.groups:
             group_name, benchmarks = group
             for benchmark in benchmarks:
-                if 'cprofile' in benchmark:
+                if "cprofile" in benchmark:
                     if not section_displayed:
-                        tr.section('cProfile (time in s)', sep='-', yellow=True)
+                        tr.section("cProfile (time in s)", sep="-", yellow=True)
                         section_displayed = True
-                    tr.write(benchmark['fullname'], yellow=True)
-                    if benchmark['source']:
-                        tr.write_line(' ({})'.format(benchmark['source']))
+                    tr.write(benchmark["fullname"], yellow=True)
+                    if benchmark["source"]:
+                        tr.write_line(" ({})".format(benchmark["source"]))
                     else:
-                        tr.write('\n')
-                    tr.write_line('ncalls\ttottime\tpercall\tcumtime\tpercall\tfilename:lineno(function)')
-                    for function_info in benchmark['cprofile']:
+                        tr.write("\n")
+                    tr.write_line("ncalls\ttottime\tpercall\tcumtime\tpercall\tfilename:lineno(function)")
+                    for function_info in benchmark["cprofile"]:
                         line = (
-                            '{ncalls_recursion}\t{tottime:.{prec}f}\t{tottime_per:.{prec}f}\t{cumtime:.{prec}f}'
-                            '\t{cumtime_per:.{prec}f}\t{function_name}'
+                            "{ncalls_recursion}\t{tottime:.{prec}f}\t{tottime_per:.{prec}f}\t{cumtime:.{prec}f}"
+                            "\t{cumtime_per:.{prec}f}\t{function_name}"
                         ).format(prec=4, **function_info)
                         tr.write_line(line)
-                    tr.write('\n')
+                    tr.write("\n")
