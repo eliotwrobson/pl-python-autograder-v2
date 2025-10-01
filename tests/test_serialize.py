56f6b2bdff516db8387e4ba0a331ca32e1de5c19
Eliot Wong Robson <eliot.robson24@gmail.com>
2025-10-01 02:34:33 -0400

    Add some tests

--- tests/test_serialize.py
+++ tests/test_serialize.py
@@ -1,7 +1,11 @@
+from typing import Any
 from typing import cast
 
 import numpy as np
+import pytest
 
+from pytest_pl_grader.json_utils import from_json
+from pytest_pl_grader.json_utils import to_json
 from pytest_pl_grader.utils import deserialize_object_unsafe
 from pytest_pl_grader.utils import serialize_object_unsafe
 
@@ -18,3 +22,14 @@ def test_serialize_numpy_array() -> None:
 
     # Check if the original and deserialized arrays are equal
     assert np.array_equal(arr, deserialized)
+
+
+@pytest.mark.parametrize("obj", [np.bool(True), np.int32(42), np.float64(3.14), complex(1, 2)])
+def test_serialize_json(obj: Any) -> None:
+    # Serialize the object to JSON-compatible format
+    json_compatible = to_json(obj)
+
+    # Deserialize back to original object
+    deserialized = from_json(json_compatible)
+
+    np.testing.assert_equal(obj, deserialized)
