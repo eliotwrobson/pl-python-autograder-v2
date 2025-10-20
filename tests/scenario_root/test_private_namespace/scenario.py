3b496ed38ff347d0eca2fafb4f5282260df187fc
Eliot Wong Robson <eliot.robson24@gmail.com>
2025-10-19 22:24:20 -0400

    Add function test

--- tests/scenario_root/test_private_namespace/scenario.py
+++ tests/scenario_root/test_private_namespace/scenario.py
@@ -10,3 +10,4 @@ def test_random(sandbox: StudentFixture) -> None:
     assert sandbox.query("c") == 42
     assert sandbox.query("a") == 10
     assert sandbox.query("b")["c"] == 50
+    assert sandbox.query("d") == 11
--- tests/scenario_root/test_private_namespace/setup_code.py
+++ tests/scenario_root/test_private_namespace/setup_code.py
@@ -4,3 +4,7 @@ super_secret_value = 10
 
 def weird_func(a):
     return a + 20
+
+
+def f(a):
+    return a + 1
